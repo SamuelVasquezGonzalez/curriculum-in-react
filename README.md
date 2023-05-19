a63bc24cd9e3c743e64c66e5e5288acd8c4e6132
SamuelVasquezGonzalez <samuegonza2006@gmail.com>
2023-05-19 16:04:50 -0500

    Be updated the script that read the api, added validations

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # To do list
 
-- Update projects card(update the img and the info, be try make with props for make more dinamic)
+- Update the projects images resolution
+- Make the Skills main
--- src/App.js
+++ src/App.js
@@ -3,19 +3,51 @@ import Banner from './components/banner'
 import Particle from './components/particle'
 import Projects from './components/Projects';
 import Bubbles from './components/bubbles';
+import StatusMsm from './components/status';
+import LoaderVideo from './assets/loader.gif'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 
 function App() {
-
   const [data, setData] = useState([]);
+  const [state, setState] = useState({modal: "no-visible", msm: "null", status: "nothing"});
 
   useEffect(() => {
     fetch('https://samuelvasquezgonzalez.github.io/API-portf/proyectos.json')
       .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error))
+      .then(response => {
+        setData(response)
+        showModal("no-visible", "null", "nothing");
+      })
+      .catch(error => {
+        setData([
+          {
+            img: LoaderVideo,
+            info: "We have had some inconveniences when loading the information :(",
+            url: '#'
+          },
+          {
+            img: LoaderVideo,
+            info: "We have had some inconveniences when loading the information :(",
+            url: '#'
+          },
+          {
+            img: LoaderVideo,
+            info: "We have had some inconveniences when loading the information :(",
+            url: '#'
+          }
+        ]);
+        if(navigator.onLine){
+          showModal("visible", "We are having issues to load the information:(", "Our API is not working at the moment, we are trying to solve it as quickly as possible ");
+        }else{
+          showModal("visible", "You don't have internet connection", "Check you internet access")
+        }
+      });
   }, []);
+
+  const showModal = (modal, msm, status) =>{
+    setState({modal: modal, msm: msm, status: status});
+  }
 
   return (
     <div className="App">
@@ -23,11 +55,11 @@ function App() {
       <Banner />
       <Particle />
       <Projects>
-      {data.map((bubble, index) => (
+        {data.map((bubble, index) => (
           <Bubbles src={bubble.img} info={bubble.info}  url={bubble.url} key={index}/>
         ))}
       </Projects>
-      
+      <StatusMsm modal={state.modal} msm={state.msm} status={state.status} />
     </div>
   );
 }
