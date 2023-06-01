118208d7f6a07287381d14374696bc46e81b182c
SamuelVasquezGonzalez <samuegonza2006@gmail.com>
2023-05-31 23:03:09 -0500

    7th attempt to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import rutas from './routes/routerController.ts';
+import rutas from './routes/routerController';
 
 function App() {
   return (
