c98b5a40c7d632bd95f2d6690eaa403d7573bcf2
SamuelGonza <samuel@tecnotics.com>
2024-04-20 20:38:01 -0500

    updated projects, employment and skills

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,32 +4,39 @@ import MiniBanner from "../resumeComponents/resumeBanner/ResumeBanner";
 import Employment from "../resumeComponents/employment/Employment";
 import Experience from "../resumeComponents/experience/Experience";
 import Inspirations from "../resumeComponents/inspirations/Inspirations";
-import Footer from '../components/footer/Footer'
+import Footer from "../components/footer/Footer";
 import { Helmet } from "react-helmet";
 
-const Resume = () =>{
+const Resume = () => {
     return (
         <>
-            <Helmet>
-                <title>Samuel Gonzalez | Resume</title>
-                <meta name="description" content="Esta es la misma informacion pero un poco mas resumida, incluso esta la historia de como comenzo todo este amor por el desarollo de soluciones y aplicaciones web, tambien las personas que me apoyaron y me inspiraron a seguir adelante" />
-                <link rel="canonical" href="https://samuelgonzalez.onrender.com/resume" />
-            </Helmet>
+            <div className="helmet_info">
+                <Helmet>
+                    <title>Samuel Gonzalez | Resume</title>
+                    <meta name="description" content="Esta es la misma informacion pero un poco mas resumida, incluso esta la historia de como comenzo todo este amor por el desarollo de soluciones y aplicaciones web, tambien las personas que me apoyaron y me inspiraron a seguir adelante"
+                    />
+                    <link
+                        rel="canonical"
+                        href="https://samuegonzalez.netlify.app/resume"
+                    />
+                </Helmet>
+            </div>
             <HeaderResume />
-            <MiniBanner bigTitle="RESUME"/>
+            <MiniBanner bigTitle="RESUME" />
             <Employment />
             <Experience />
             <Inspirations />
-                <div className="img-container">
-                    <img
+            <div className="img-container">
+                <img
                     className="img-footer"
                     src="https://samuelvasquezgonzalez.github.io/API-portf/multimedia/logoVector.svg"
-                    alt="Samuel Gonzalez Logo" title="Samuel Gonzalez Logo"
-                    />
-                </div>
+                    alt="Samuel Gonzalez Logo"
+                    title="Samuel Gonzalez Logo"
+                />
+            </div>
             <Footer />
         </>
-    )
-}
+    );
+};
 
 export default Resume;
--- src/resumeComponents/employment/Employment.jsx
+++ src/resumeComponents/employment/Employment.jsx
@@ -8,15 +8,22 @@ function Employment() {
             <h2 className="sectionTitle">EMPLOYMENT</h2>
             <div className="job">
                 <h3>FREELANCER</h3>
-                <p className="pNormal centrado">Backend Developer | Front-end Developer</p>
+                <p className="pNormal centrado">Full-Stack web Developer</p>
                 <p className="pNormal centrado">I have freelanced on several projects prior to creating WhiteForest, which I now take precedence over freelancing</p>
                 <NavLink className="a-style" to="/employment/freelancer">View projects »</NavLink>
                 <p className="pNormal centrado">2022-</p>
             </div>
             <div className="job">
-                <h3>SA-MN</h3>
-                <p className="pNormal centrado">Backend Developer | Front-end Developer</p>
+                <h3>ENIGLOGIC (personal project)</h3>
+                <p className="pNormal centrado">Full-Stack web Developer</p>
                 <p className="pNormal centrado">From creating the database with MySql, going through the endpoints with Node, and making components and validating tokens with React.js</p>
+                <p className="pNormal centrado">2023-</p>
+            </div>
+            <div className="job">
+                <h3>TECNOTICS</h3>
+                <p className="pNormal centrado">Full-Stack web Developer</p>
+                <p className="pNormal centrado">I work as a full-stack, from the backend with Node/Express/MongoDb and on the frontend with ReactJs+vite/redux.</p>
+                <p className="pNormal centrado">Of these two areas, I use any subrole, whether for maintenance, bug fixes or implementation of new features.</p>
                 <p className="pNormal centrado">2023-</p>
             </div>
         </main>
